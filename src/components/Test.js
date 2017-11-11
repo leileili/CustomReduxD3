62db4532cf8d84c662f58204d6513f204eda2302
leileili <leileili@hotmail.com>
2017-11-10 20:46:42 -0800

    init

---
+++ src/components/Test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HeaderContainer from './HeaderContainer'
+
+
+export default class Test extends React.Component {
+  // render
+  render() {
+    return (
+  		
+      <div >      
+      	Test page
+      </div>
+    );
+  }
+}
+
+
