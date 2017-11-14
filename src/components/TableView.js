23e821967536930b44bbbc1eec44630771567d13
leileili <leileili@hotmail.com>
2017-11-13 17:06:16 -0800

    add

--- src/components/TableView.js
+++ src/components/TableView.js
@@ -7,8 +7,8 @@ import cm from '../common/CommunicationManager'
 export default class TableView extends React.Component{
 	render() {
 		return (
-				<div style={{"height":"300px", "background":"#FFF"}}>
-					<p> Here are the data loaded from a json file.</p>
+				<div style={{"height":"300px"}}>
+				
 					<BootstrapTable data={ this.props.data } maxHeight="400px" hover={true} striped={true}>
 				        <TableHeaderColumn dataField='name' isKey>name</TableHeaderColumn>
 				        <TableHeaderColumn dataField='shape_leng'>shape_leng</TableHeaderColumn>
